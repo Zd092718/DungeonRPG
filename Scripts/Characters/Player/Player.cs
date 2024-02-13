380b659f979dbc0caacec92a36cf6fde2ae250b1
Zack Dowd <zdowd2796@gmail.com>
2024-02-13 13:36:30 -0700

    player movement working

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,18 +3,49 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimatedSprite3D animSprite;
+    [Export] private Camera3D camera;
     private Vector2 direction = new();
+
+    public override void _Ready()
+    {
+
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(direction.X, 0, direction.Y);
         Velocity *= 5;
+
         MoveAndSlide();
+
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(
-            "MoveLeft", "MoveRight", "MoveForward", "MoveBackward"
+            GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD
         );
+
+        if (direction == Vector2.Zero)
+        {
+            animSprite.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            animSprite.Play(GameConstants.ANIM_MOVE);
+        }
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally) { return; }
+
+        bool isMovingLeft = Velocity.X < 0;
+        animSprite.FlipH = isMovingLeft;
     }
 }
