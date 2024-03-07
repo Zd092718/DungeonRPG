bd7df2d873427e2497dfd00d146ec117ae8fd2c2
Zack Dowd <zdowd2796@gmail.com>
2024-03-07 13:48:06 -0700

    created stat resource for stats

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -2,17 +2,30 @@
 
 public abstract partial class Character : CharacterBody3D
 {
+    [Export] private StatResource[] stats;
+    
     [ExportGroup("Required Nodes")]
     [Export] public AnimatedSprite3D AnimSprite { get; private set; }
     [Export] public StateMachine StateMachineNode { get; private set; }
+    [Export] public Area3D HurtboxAreaNode { get; private set; }
 
     [ExportGroup("AI Nodes")]
     [Export] public Path3D PathNode { get; private set; }
     [Export] public NavigationAgent3D AgentNode { get; private set; }
     [Export] public Area3D ChaseAreaNode { get; private set; }
     [Export] public Area3D AttackAreaNode { get; private set; }
+    
 
     public Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        HurtboxAreaNode.AreaEntered += HandleHurtboxAreaEntered;
+    }
+    private void HandleHurtboxAreaEntered(Area3D area)
+    {
+        GD.Print($"{area.Name} hit");
+    }
 
     public void Flip()
     {
